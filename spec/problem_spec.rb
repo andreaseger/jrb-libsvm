bdce70b82b2149beebd40f871ecd53a5894174a7
Andreas Eger <dev@eger-andreas.de>
2013-01-27 02:44:00 +0100

    small spec change [ci skip]

--- spec/problem_spec.rb
+++ spec/problem_spec.rb
@@ -24,14 +24,14 @@ describe Problem do
                 Node.features(0.1,0.5,0.1,0.9),
                 Node.features(0.2,0.2,0.6,0.5),
                 Node.features(0.3,0.1,0.5,0.9)]
-    @problem.set_examples([1,2,1,2], examples)
+    ->{@problem.set_examples([1,2,1,2], examples)}.should_not raise_error
   end
 
   it "can be set twice over" do
     features = [Node.features(0.2, 0.3, 0.4, 0.4), Node.features(0.3,0.1,0.5,0.9)]
     @problem.set_examples([1,2], features)
     features = [Node.features(0.2, 0.3, 0.4, 0.4), Node.features(0.3,0.1,0.5,0.9)]
-    @problem.set_examples([8,2], features)
+    ->{@problem.set_examples([8,2], features)}.should_not raise_error
   end
 
 end
